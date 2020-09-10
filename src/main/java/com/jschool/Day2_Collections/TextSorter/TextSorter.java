5082c6e68092d8eec198a04f016924d50f6edbde
Alexandr Ryabtsev <awryabtsev@GMail.com>
2020-09-10 10:31:21 +0300

    Task 2.4 done!

--- src/main/java/com/jschool/Day2_Collections/TextSorter/TextSorter.java
+++ src/main/java/com/jschool/Day2_Collections/TextSorter/TextSorter.java
@@ -11,21 +11,33 @@ import java.util.*;
 public class TextSorter {
     public static void main(String[] args) throws IOException {
         File file = new File("C:/Users/16727641/IdeaProjects/JavaSchoolSberbank/ResourcesFiles/Day2_test.txt");
-        System.out.println(textHolder(file));
+//        System.out.println(textHolder(file));
+//
+//        System.out.println("-------------\nКол-во слов: " + wordCounter(textHolder(file)));
+//
+//        System.out.println("-----------");
 
-        System.out.println("-------------\nКол-во слов: " + wordCounter(textHolder(file)));
+//        for (String s : sortedUniqueWords(textHolder(file))) {
+//            System.out.println(s);
+//        }
 
-        System.out.println("-----------");
+//        for (Map.Entry<String, Integer> entry : uniqueWordCounter(textHolder(file)).entrySet()) {
+//            System.out.println("Key: " + entry.getKey() + " Value: " + entry.getValue());
+//        }
 
-        for (String s : sortedUniqueWords(textHolder(file))) {
-            System.out.println(s);
-        }
+
+//        for (String s : reversStrings(textHolder(file))) {
+//            System.out.println(s);
+//        }
+     //   printReversStrings(textHolder(file));
+        //sortedUniqueWords(textHolder(file));
+
 
 
     }
 
 
-    //TODO: добавить нормализацию, чтобы возвращались только слова, без чисел и знаков препинания.
+    //TODO: добавить нормализацию, чтобы возвращались только слова, без чисел знаков препинания и переноса строки.
     public static String textHolder(File file) {
 
         List<String> allLines = new ArrayList<>();
@@ -46,14 +58,14 @@ public class TextSorter {
     }
 
     //Подсчитайте количество различных слов в файле
-    public static int wordCounter(String text) {
+    public static void wordCounter(String text) {
         Set<String> wordSet = new HashSet<>(Arrays.asList(text.split(" ")));
-        return wordSet.size();
+        System.out.println("Количество уникальныйх слов: " + wordSet.size() + "\n");
     }
 
     //Выведите на экран список различных слов файла, отсортированный по возрастанию их длины
     // (компаратор сначала по длине слова, потом по тексту)
-    public static TreeSet<String> sortedUniqueWords(String textString) {
+    public static void sortedUniqueWords(String textString) {
 
         Comparator<String> lengthCompare = new Comparator<String>() {
             @Override
@@ -72,7 +84,36 @@ public class TextSorter {
         TreeSet<String> stringTreeSet = new TreeSet<>(lengthCompare.thenComparing(textCompare));
         stringTreeSet.addAll(Arrays.asList(textString.split(" ")));
 
-        return stringTreeSet;
+        stringTreeSet.forEach(System.out::println);
+
+    }
+
+    //Подсчитайте и выведите на экран сколько раз каждое слово встречается в файле.
+    public static Map<String, Integer> uniqueWordCounter(String textString) {
+        HashMap<String, Integer> resultMap = new HashMap<>();
+
+        for (String s : textString.split(" ")) {
+            if (!resultMap.containsKey(s)) {
+                resultMap.put(s, 1);
+            } else {
+                Integer i = resultMap.get(s) + 1;
+                resultMap.put(s, i);
+            }
+        }
+        return resultMap;
+    }
+
+    //Выведите на экран все строки файла в обратном порядке.
+    public static void printReversStrings(String textString) {
+
+        Deque<String> stringDeque = new ArrayDeque<>();
+         for (String s : textString.split(System.lineSeparator())) {
+             stringDeque.push(s);
+         }
+
+         for (String s : stringDeque) {
+             System.out.println(s);
+         }
 
     }
 
