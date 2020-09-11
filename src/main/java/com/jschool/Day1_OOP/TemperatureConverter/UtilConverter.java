c750e0381ba27177652ba7c3116094f87353af75
Alexandr Ryabtsev <awryabtsev@GMail.com>
2020-09-11 14:21:21 +0300

    JavaDoc comments where added.

---
+++ src/main/java/com/jschool/Day1_OOP/TemperatureConverter/UtilConverter.java
@@ -0,0 +1,36 @@
+package com.jschool.Day1_OOP.TemperatureConverter;
+
+public class UtilConverter {
+
+    // Создан утилитарный класс для конвертации температуры.
+    private static class Converter {
+
+        public static double toFahrenheit(double celsius) {
+            return celsius * (9.0 / 5) + 32;
+        }
+
+        public static double toKelvin(double celsius) {
+            return celsius + 273.15 * 100;
+        }
+
+        public static double toRankin(double celsius) {
+            return (celsius + 273.15) * (9.0 / 5) * 100;
+        }
+
+        public static double toDelisle(double celsius) {
+            return (100 - celsius) * (3.0 / 2) * 1000;
+        }
+
+        public static double toNewton(double celsius) {
+            return celsius * (33.0 / 100) * 100;
+        }
+
+        public static double toReaumur(double celsius) {
+            return celsius * (4.0 / 5) * 100;
+        }
+
+        public static double toRomer(double celsius) {
+            return celsius * (21.0 / 40) + 7.5 * 100;
+        }
+    }
+}
---
+++ src/main/java/com/jschool/Day2_Collections/TextSorter/PrepareString.java
@@ -0,0 +1,37 @@
+package com.jschool.Day2_Collections.TextSorter;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrepareString {
+    public static final File FILE = new File("src/main/resources/Day2_test.txt");
+
+    /**Служебный класс. Используется для получения строки из файла, для ее дальнейшей сортировки.
+     *
+     * @param file
+     * @return String
+     */
+    public static String textHolder(File file) {
+
+        List<String> allLines = new ArrayList<>();
+
+        try {
+            allLines = Files.readAllLines(Paths.get(file.getAbsolutePath()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String s : allLines) {
+            stringBuilder.append(s);
+            stringBuilder.append(System.lineSeparator());
+        }
+
+        return stringBuilder.toString().replaceAll("\\p{Punct}|\\d", "").toLowerCase().
+                replaceAll(System.lineSeparator(), "");
+    }
+}
