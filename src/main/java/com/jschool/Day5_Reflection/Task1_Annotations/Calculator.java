95ba8ea9069455b8c355140d2e4281528681141a
Alexandr Ryabtsev <awryabtsev@GMail.com>
2020-09-21 11:38:15 +0300

    Day5(Reflection) - Tasks 1-4 done!

---
+++ src/main/java/com/jschool/Day5_Reflection/Task1_Annotations/Calculator.java
@@ -0,0 +1,10 @@
+package com.jschool.Day5_Reflection.Task1_Annotations;
+
+public interface Calculator {
+    /**
+     * Расчет факториала числа.
+     * @param number - натуральное число, факториал которого будет вычислен.
+     */
+    int calc (int number);
+
+}
